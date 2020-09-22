06caf4f746372b3c03ab56ed498ae5aab06b2aec
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-21 20:42:00 -0700

    Changed SFRDNV validation

--- SFRDNV .R
+++ SFRDNV .R
@@ -1,4 +1,7 @@
-SFRDNV <- function(a,b,w,epsilon=0.01){
-  stopifnot(numeric(a), numeric(b), numeric(w));
+SFRDNV <- function(a,b,w,epsilon=0.01) {
+  if(!numeric(a) | !numeric(b) | !numeric(w)){
+    return -1;
+  }
   
 }
+
