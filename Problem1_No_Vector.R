8fc8faf6dbb8aeeb14306b924428b544d2092c8c
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-21 21:04:30 -0700

    Added less than 0 validity check

--- Problem1_No_Vector.R
+++
@@ -1,2 +0,0 @@
-#Here we will put down problem 1 no vector
-
--- SFRDNV .R
+++ SFRDNV .R
@@ -2,6 +2,10 @@ SFRDNV <- function(a,b,w,epsilon=0.01) {
   if(!numeric(a) | !numeric(b) | !numeric(w)){
     return -1;
   }
+  if(a < 0  | b < 0 | w < 0 | epsilon < 0){
+    return -1;
+  }
+  
+  
   
 }
-
