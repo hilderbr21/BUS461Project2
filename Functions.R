72771e4d0a599771521a8ef8b3bd58b6363ca9c5
jcrowley1998 <crowleyj21@up.edu>
2020-09-22 21:08:10 -0700

    Added 4 New Functions

---
+++ Functions.R
@@ -0,0 +1,4 @@
+SFRDNV <- function(a,b,w,epsilon=0.01){
+  stopifnot(numeric(a), numeric(b), numeric(w));
+  
+}
--- SFEDNV.R
+++ SFEDNV.R
@@ -1,4 +1,4 @@
-SFRDNV <- function(a,b,w,epsilon=0.01) {
+SFEDNV <- function(a,b,w,epsilon=0.01) {
   if(!numeric(a) | !numeric(b) | !numeric(w)){
     return -1;
   }
