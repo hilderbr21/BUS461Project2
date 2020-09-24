731ba6359774fdf41526c5aa08a4e384b1268254
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-23 21:18:39 -0700

    Created Unit Test Functions

--- UnitTests.R
+++ UnitTests.R
@@ -1,2 +1,23 @@
 #Unit Test
 
+require(RUnit)
+
+TestSFRDNV<- function(){
+  
+}
+
+TestSFEDNV<- function(){
+  
+}
+
+TestSFRDV<- function(){
+  
+}
+
+TestSFEDV<- function(){
+  
+}
+
+TestB1Policy<- function(){
+  
+}
