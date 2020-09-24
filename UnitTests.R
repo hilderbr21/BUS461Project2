7cb74e220e2c12b4342b1a1c311f654dd0ab2578
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-23 22:48:10 -0700

    Update UnitTests.R
    Update invalid data type in SFRDNV

--- UnitTests.R
+++ UnitTests.R
@@ -4,9 +4,7 @@ require(RUnit)
 
 TestSFRDNV<- function(){
   
-  #manually calculate correct answer for x, y, total cost for SFRDNV
-  
-  
+  #starting values
   a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
   b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
   w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
@@ -21,7 +19,7 @@ TestSFRDNV<- function(){
   
   #check for invalid arguments
   #1. invalid datatype
-  checkEquals(-1, SFRDNV(a,b,w))
+  checkEquals(-1, SFRDNV("Gary Mitchell is the best professor!"))
   #2. inconsistent vector sizes
   checkEquals(-1, SFRDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5)))
   #3. negative number in one of the vectors
