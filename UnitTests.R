ec8427ab0b16ba8374a0ef6f7821c1dc3e69d3bc
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-23 22:33:41 -0700

    added correct answer unit test in SFRDNV

--- UnitTests.R
+++ UnitTests.R
@@ -10,8 +10,14 @@ TestSFRDNV<- function(){
   a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
   b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
   w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
+  TC<-1175
   
-  #checkEquals()
+  #check with correct answer
+  #default epsilon
+  checkEquals(c(6,8,1175),SFRDNV(a,b,w))
+  #modified epsilon
+  checkEquals(c(6,8,1175),SFRDNV(a,b,w,epsilon=0.001))
+  
   
   #check for invalid arguments
   #1. invalid datatype
