5aaa80302117e57eb7c7dbd337471c20638b05b8
jcrowley1998 <crowleyj21@up.edu>
2020-09-23 22:43:04 -0700

    Update UnitTests.R
    Added what was given in TestSFRDNV function up until TC<-1175

--- UnitTests.R
+++ UnitTests.R
@@ -40,14 +40,26 @@ TestSFEDNV<- function(){
   a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
   b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
   w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
-  x(new_coord)<-0
-  y(new_coord)<-0
+  TC<-1175
+  
+  #check with correct answer
+  #default epsilon
+  checkEquals(c(6,8,1175),SFRDNV(a,b,w))
+  #modified epsilon
+  checkEquals(c(6,8,1175),SFRDNV(a,b,w,epsilon=0.001))
+  x(new_coord)<-0 #will change this value to "x" in the future
+  y(new_coord)<-0 #will change this value to "y" in the future
   total_cost<-0
   
   #check for invalid arguments
   #1. invalid datatype
   checkEquals(-1, SFEDNV(x(new_coord), y(new_coord), total_cost))
-  #2. 
+  #2. inconsistent vector sizes
+  checkEquals(-1, SFRDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5)))
+  #3. negative number in one of the vectors
+  checkEquals(-1, SFRDNV(c(1,2,3,-4),c(1,2,3,4),c(1,2,3,4)))
+  #4. negative scalar precision
+  checkEquals(-1, SFRDNV(c(1,2,3,4),c(1,2,3,4),c(1,2,3,4),-0.5)) 
 }
 
 TestSFRDV<- function(){
