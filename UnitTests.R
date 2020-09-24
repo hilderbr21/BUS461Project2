b9841deb1b84221765e4221fa61c1d763411272a
jcrowley1998 <crowleyj21@up.edu>
2020-09-23 22:11:19 -0700

    Changes to UnitTests and SFRDV
    SFRDV: Added function() and if statements validating the inputs
    UnitTests: Made changes to SFEDNV regarding the RUnit "checkEquals"

--- UnitTests.R
+++ UnitTests.R
@@ -7,7 +7,19 @@ TestSFRDNV<- function(){
 }
 
 TestSFEDNV<- function(){
+  #manually name new coordinates x(new_coord), y(new_coord)
+  #calculate total_cost
+  #manually test converged 
+  #create a data.frame called data.frame.SFEDNV containing: 
+  #"x(new_coord)", "y(new_coord)", and "total_cost"
+  x(new_coord)<-0
+  y(new_coord)<-0
+  total_cost<-0
   
+  #check for invalid arguments
+  #1. invalid datatype
+  checkEquals(-1, SFEDNV(x(new_coord), y(new_coord), total_cost))
+  #2. 
 }
 
 TestSFRDV<- function(){
