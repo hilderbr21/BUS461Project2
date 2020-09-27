581569af6ab365e62e9c86fd70f22e9799d79a0c
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-27 14:21:34 -0700

    Reimagined unit test system
    Completed SFRDNV unit test

--- SFRDNV.R
+++ SFRDNV.R
@@ -1,14 +1,20 @@
 SFRDNV <- function(a,b,w,epsilon=0.01) {
-  if(!is.numeric(a) | !is.numeric(b) | !is.numeric(w)){
-    return -1;
+  
+  if(!is.vector(a, mode="numeric") | !is.vector(b, mode="numeric") | !is.vector(w, mode="numeric")){
+    return(-1)
   }
-  if(a < 0  | b < 0 | w < 0 | epsilon < 0){
-    return -1;
+  if(length(a) != length(b) | length(b) != length(w) | length(w) != length(a)){
+    return(-1)
+  }
+  for(i in 1:length(a))
+    if(a[i] < 0 | b[i] < 0 | w[i] < 0 | epsilon < 0){
+      return( -1)
   }
   
   
   
-  sortingMatrix <- matrix(a,b,w, dimnames=c('a','b','w'))
-  print(sortingMatrix)
+  
+  #sortingMatrix <- matrix(a,b,w, dimnames=c('a','b','w'))
+  #print(sortingMatrix)
 }
   
