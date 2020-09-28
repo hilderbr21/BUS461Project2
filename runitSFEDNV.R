319384665135dd98956b688fe08f17447d971cee
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-28 02:30:50 -0700

    Added correct answers to ruintiSFEDNV

--- runitSFEDNV.R
+++ runitSFEDNV.R
@@ -12,21 +12,27 @@ source("SFEDNV.R")
 a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
 b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
 w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
-TC<-1175
+iterlimit<-14
+
+x<-7.7116
+y<-7.2385
+TC<-2011.2
+convergance<-TRUE
+iterations<-14 
 
   
 test.SFEDNVvalid<- function(){
   #check with correct answer
   #default epsilon
-  checkEquals(c(6,8,1175),SFEDNV(a,b,w))
+  checkEquals(c(x,y,TC,convergance,iterations),SFEDNV(a,b,w,interlimit))
   #modified epsilon
-  checkEquals(c(6,8,1175),SFEDNV(a,b,w,epsilon=0.001))
+  checkEquals(c(x,y,TC,FALSE,14),SFEDNV(a,b,w,epsilon=0.001,interlimit))
 }
   
 test.SFEDNVinvalid<- function(){
   #check for invalid arguments
   #1. invalid datatype
-  checkEquals(-1, SFEDNV(x(new), y(new), total_cost))
+  checkEquals(-1, SFEDNV("Please have mercy on our grades Dr. Mitchell.", y,w,iterlimit))
   #2. inconsistent vector sizes
   checkEquals(-1, SFEDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5)))
   #3. negative number in one of the vectors
