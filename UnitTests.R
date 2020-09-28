f83033f4b7d13545b02fb28c1ca22e24c8e914e3
jcrowley1998 <crowleyj21@up.edu>
2020-09-27 21:07:32 -0700

    Changes to the codes

--- UnitTests.R
+++ UnitTests.R
@@ -37,13 +37,11 @@ test.SFRDNVinvalid<- function(){
   checkEquals(-1, SFRDNV(c(1,2,3,4),c(1,2,3,4),c(1,2,3,4),-0.5))
   
 }
+source("SFEDNV.R")
 
 TestSFEDNV<- function(){
-  #manually name new coordinates x(new_coord), y(new_coord)
-  #calculate total_cost
-  #manually test converged 
-  #create a data.frame called data.frame.SFEDNV containing: 
-  #"x(new_coord)", "y(new_coord)", and "total_cost"
+  
+  #starting values
   a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
   b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
   w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
@@ -57,13 +55,13 @@ TestSFEDNV<- function(){
   
   #check for invalid arguments
   #1. invalid datatype
-  checkEquals(-1, SFEDNV(x(new_coord), y(new_coord), total_cost))
+  checkEquals(-1, SFEDNV(x(new), y(new), total_cost))
   #2. inconsistent vector sizes
-  checkEquals(-1, SFRDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5)))
+  checkEquals(-1, SFEDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5)))
   #3. negative number in one of the vectors
-  checkEquals(-1, SFRDNV(c(1,2,3,-4),c(1,2,3,4),c(1,2,3,4)))
+  checkEquals(-1, SFEDNV(c(1,2,3,-4),c(1,2,3,4),c(1,2,3,4)))
   #4. negative scalar precision
-  checkEquals(-1, SFRDNV(c(1,2,3,4),c(1,2,3,4),c(1,2,3,4),-0.5)) 
+  checkEquals(-1, SFEDNV(c(1,2,3,4),c(1,2,3,4),c(1,2,3,4),-0.5)) 
 }
 
 TestSFRDV<- function(){
