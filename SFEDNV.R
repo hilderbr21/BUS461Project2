ce016295b8c053d97d5e4b9ca429f27732765695
Marc.Hbrand <54870346+hilderbr21@users.noreply.github.com>
2020-09-27 21:10:10 -0700

    Added interlimit arg to SFEDNV

--- SFEDNV.R
+++ SFEDNV.R
@@ -1,14 +1,18 @@
-SFEDNV <- function(a,b,w,epsilon=0.01) {
+#SFRDNV (problem 1 no vector)
+#@author Joe Crowley
+
+SFEDNV <- function(a,b,w,epsilon=0.01,interlimit) {
   
   #validity check
-  if(!is.vector(a, mode="numeric") | !is.vector(b, mode="numeric") | !is.vector(w, mode="numeric")){
+  if(!is.vector(a, mode="numeric") | !is.vector(b, mode="numeric") | !is.vector(w, mode="numeric")
+     | !is.numeric(interlimit)){
     return(-1)
   }
   if(length(a) != length(b) | length(b) != length(w) | length(w) != length(a)){
     return(-1)
   }
   for(i in 1:length(a))
-    if(a[i] < 0 | b[i] < 0 | w[i] < 0 | epsilon < 0){
+    if(a[i] < 0 | b[i] < 0 | w[i] < 0 | epsilon < 0 | interlimit < 0){
       return( -1)
   }
   
