a7f69f9e5269902e1a7e99cf3a5ecc4d0c0273bd
jcrowley1998 <crowleyj21@up.edu>
2020-09-27 22:33:07 -0700

    Script Changes
    Change SFRDNV and added to B1Policy

---
+++ B1Policy.R
@@ -0,0 +1,15 @@
+SFEDNV <- function(D,A,v,r) {
+  
+  #validity check
+  if(!is.vector(D, mode="numeric") | !is.vector(A, mode="numeric") | !is.vector(v, mode="numeric") | !is.vector(r, mode="numeric")){
+    return(-1)
+  }
+  if(length(a) != length(b) | length(b) != length(w) | length(w) != length(a)){
+    return(-1)
+  }
+  for(i in 1:length(a))
+    if(a[i] < 0 | b[i] < 0 | w[i] < 0 | epsilon < 0){
+      return( -1)
+    }
+  
+}
