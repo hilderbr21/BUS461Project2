a63c629c8af18ac93ec0b53ea4bf24fc609cd000
jcrowley1998 <crowleyj21@up.edu>
2020-10-03 15:31:25 -0700

    Created SFEDV page

--- SFEDV.R
+++ SFEDV.R
@@ -1,12 +1,50 @@
-SFEDNV <- function(a,b,w,epsilon=0.01) {
-  if(!numeric(a) | !numeric(b) | !numeric(w)){
-    return -1;
+#SFEDV (problem 2 vector)
+#@author Joe Crowley
+
+SFEDV <- function(a,b,w,epsilon=0.01,iterlimit) {
+  
+  #validity check
+  if(!is.vector(a, mode="numeric") | !is.vector(b, mode="numeric") | !is.vector(w, mode="numeric")
+     | !is.numeric(iterlimit)){
+    return(-1)
   }
-  if(a < 0  | b < 0 | w < 0 | epsilon < 0){
-    return -1;
+  if(length(a) != length(b) | length(b) != length(w) | length(w) != length(a)){
+    return(-1)
+  }
+  for(i in 1:length(a))
+    if(a[i] < 0 | b[i] < 0 | w[i] < 0 | epsilon < 0 | iterlimit < 0){
+      return( -1)
+    }
+  
+  
+  # calculate x0 and y0 using calcCentroid 
+  x<-0
+  y<-0
+  xNumerTemp<-0
+  xDenomTemp<-0
+  yNumerTemp<-0
+  yDenomTemp<-0
+  
+  # calculate revised coordinates
+  for(j in 2:(iterlimit + 1)){
+    xNumerTemp<- xNumerTemp + (w[j]*a[j])
+    xDenomTemp <- xDenomTemp + w[j]
+    yNumerTemp <- yNumerTemp + (w[j]*b[j])
+    yDenomTemp <- yDenomTemp + w[j]
+    x[j] <-xNumerTemp/xDenomTemp
+    y[j] <-yNumerTemp/yDenomTemp
+    iterations <- j-1
+    
+    # test for convergence  
+    
+    if(abs((x[j] - x[j-1])) <= epsilon) 
+      if((y[j] - y[j-1]) <= epsilon){
+        
+        TC<-w*sqrt((abs(x-a) + abs(y-b)))
+      } else {
+        return(-1)
+      }
+    return(c(x, y, TC, convergance, iterations))
   }
   
-}
-
-
-
+} 
---
+++ runitSFEDV.R
@@ -0,0 +1,44 @@
+#Unit Test for SFEDNV (Problem 2 no vector)
+#@Author Joe Crowley
+
+if (!suppressMessages(require(RUnit)))
+{
+  stop("Terminating... required package RUnit is not installed.")
+}
+
+source("SFEDV.R")
+
+#starting values
+a<-c(2,6,9,12,3,6,1,6,10,10,2,1,4,4)
+b<-c(3,1,9,5,9,2,6,4,3,8,8,4,5,2)
+w<-c(20,10,45,15,25,5,15,10,5,40,5,5,5,5)
+iterlimit<-14
+
+x<-7.7116
+y<-7.2385
+TC<-2011.2
+convergance<-TRUE
+iterations<-14 
+
+
+test.SFEDNVvalid<- function(){
+  #check with correct answer
+  #default epsilon
+  checkEquals(c(x,y,TC,convergance,iterations),SFEDNV(a,b,w,iterlimit=iterlimit))
+  #modified epsilon
+  checkEquals(c(x,y,TC,FALSE,14),SFEDNV(a,b,w,epsilon=0.001,iterlimit=iterlimit))
+}
+
+test.SFEDNVinvalid<- function(){
+  #check for invalid arguments
+  #1. invalid datatype
+  checkEquals(-1, SFEDNV("This is a test.", b,w,iterlimit=iterlimit))
+  #2. inconsistent vector sizes
+  checkEquals(-1, SFEDNV(c(1,2,3,4,5,6),c(1,2,4,5),c(1,2,3,4,5),iterlimit=iterlimit))
+  #3. negative number in one of the vectors
+  checkEquals(-1, SFEDNV(c(1,2,3,-4),c(1,2,3,4),c(1,2,3,4),iterlimit=iterlimit))
+  #4. negative scalar precision
+  checkEquals(-1, SFEDNV(c(1,2,3,4),c(1,2,3,4),c(1,2,3,4),-0.5,iterlimit=iterlimit))
+  #5. negative iterlimit
+  checkEquals(-1, SFEDNV(c(iterlimit=-4)))
+}
